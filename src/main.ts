91f9db414de3903a5dc148959f1e9edc52122408
romesh-abeyawardena <romesh.abeyawardena@bellrockgroup.co.uk>
2023-03-20 10:32:12 +0000

    Added eruda and missing sass dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import { createApp } from 'vue';
 import { createPinia } from "pinia";
+import eruda from "eruda";
 import './scss/index.scss';
 import App from './App.vue';
+
+
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    if (!/eruda=true/.test(window.location.toString()) && localStorage.getItem('active-eruda') != 'true') return;
+    eruda.init();    
+});
 
 createApp(App)
 .use(createPinia())
