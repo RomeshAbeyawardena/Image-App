b2559baa4ee85ef5b8a9c99bd5aff011a39e242c
romesh-abeyawardena <romesh.abeyawardena@bellrockgroup.co.uk>
2023-03-23 14:23:48 +0000

    Added animations

--- src/components/bottom-nav.vue
+++ src/components/bottom-nav.vue
@@ -6,16 +6,18 @@ const { fullScreenMode, showBottomToolbar } = storeToRefs(store);
 
 </script>
 <template>
-    <div class="btn-bottom-tab" v-if="!fullScreenMode && !showBottomToolbar">
-        <a class="btn" @click="$event => showBottomToolbar = true" href="#">
-            <font-awesome-icon icon="circle-chevron-up" />
-        </a>
-    </div>
-    <div class="bottom-nav" v-if="!fullScreenMode && showBottomToolbar">
-        <h3>SX Viewer</h3>
-        <div class="spacer">&nbsp;</div>
-        <a class="btn btn-hide" @click="$event => showBottomToolbar = false" href="#">
-            <font-awesome-icon icon="circle-chevron-down" />
-        </a>
-    </div>
+    <TransitionGroup>
+        <div class="btn-bottom-tab bottom-group" v-if="!fullScreenMode && !showBottomToolbar">
+            <a class="btn" @click="$event => showBottomToolbar = true" href="#">
+                <font-awesome-icon icon="circle-chevron-up" />
+            </a>
+        </div>
+        <div class="bottom-nav bottom-group" v-if="!fullScreenMode && showBottomToolbar">
+            <h3>SX Viewer</h3>
+            <div class="spacer">&nbsp;</div>
+            <a class="btn btn-hide" @click="$event => showBottomToolbar = false" href="#">
+                <font-awesome-icon icon="circle-chevron-down" />
+            </a>
+        </div>
+    </TransitionGroup>
 </template>
