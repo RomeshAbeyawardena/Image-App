59e72fce398e625ec0b65fd5acd6cb36c5abc0b3
romeshabeyawardena <RomeshAbeyawardena@users.noreply.github.com>
2023-03-26 00:06:55 +0000

    wip

--- src/components/swipe-image.vue
+++ src/components/swipe-image.vue
@@ -5,10 +5,11 @@ import SwiperCore, { Virtual } from 'swiper';
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { onBeforeMount, ref, watch } from "vue";
 import { createimageStore } from '../stores/image-store';
-import { Iterator } from '../iterator';
+import collect from "collect.js";
 import { IImageFile } from '../image';
 import { createMainStore } from '../stores/main';
 import { EventSource } from '../event-data';
+import { Dom7 } from 'dom7';
 
 SwiperCore.use([Virtual]);
 
@@ -18,29 +19,61 @@ const imageStore = createimageStore();
 const { fileIndex, imageFiles, lastEvent } = storeToRefs(imageStore);
 const cachedImages = ref<Array<IImageFile>>();
 
-const swiperInstance = ref<any>(null);
+const swiperInstance = ref<SwiperCore>();
 
-let getNextImages: () => Array<IImageFile>;
+function getImages(next: boolean, currentIndex: number, takeCurrent: boolean) {
+    const c = collect(imageFiles.value);
+    console.log(currentIndex);
+    const m = currentIndex == 0 ? c : c.skip(takeCurrent ? currentIndex : currentIndex + 1);
+    const a = m!.take(next ? 3 : -3).toArray<IImageFile>();
+    return a;
+}
 
-watch(lastEvent, (newValue) => {
+watch(lastEvent, (newValue, oldValue) => {
     //console.log(swiperInstance);
-    if(newValue != undefined 
+    if (oldValue != undefined
+        && newValue != undefined
         && !isNaN(newValue.value)
-        && newValue.source == EventSource.menu)
-    {
-        const nextImages = Iterator<IImageFile>(imageFiles.value, 3, newValue.value);
-        cachedImages.value = nextImages();
+        && newValue.source == EventSource.menu) {
+        if (cachedImages.value?.at(newValue.value) == undefined) {
+            populateImageFiles(getImages(true, newValue.value, true));
+            console.log(newValue.value, cachedImages.value?.filter(f => f != undefined));
+        }
+         if (swiperInstance.value != undefined) {
+        //     console.log(newValue.value);
+        //     const el = document.querySelector(".image-placeholder[alt='" + newValue + "']");
+
+        //     if (el != null) {
+        //         (el.parentElement as HTMLElement).parentElement?.parentElement;
+        //     }
+            setTimeout(() => swiperInstance.value!.slideTo(newValue.value), 1000);
+        }
+        //const images = nextImages();
+        //cachedImages.value?.splice(0, 0, ...images);
     }
     //else 
-      //  console.log("bypassed");
+    //  console.log("bypassed");
 });
+
+function populateImageFiles(imageFiles: IImageFile[]) {
+    let firstElement: IImageFile | undefined;
+    //console.log("populateImageFiles");
+    if (cachedImages.value == undefined || (firstElement = imageFiles.at(0)) == undefined) {
+        //console.log("Failed");
+        return;
+    }
+    //console.log(firstElement.index);
+    cachedImages.value.splice(firstElement.index, imageFiles.length, ...imageFiles);
+}
 
 onBeforeMount(async () => {
     isLoading.value = true;
     await imageStore.getFiles("files2.json");
-    getNextImages = Iterator<IImageFile>(imageFiles.value, 3);
-    cachedImages.value = getNextImages();
+
+    cachedImages.value = new Array<IImageFile>(imageFiles.value.length);
+    populateImageFiles(getImages(true, 0, false));
     isLoading.value = false;
+    imageStore.setLastEvent(EventSource.swipe, fileIndex);
 });
 
 function loadImages(swiper: SwiperCore) {
@@ -49,11 +82,17 @@ function loadImages(swiper: SwiperCore) {
         return;
     }
     imageStore.setLastEvent(EventSource.swipe, fileIndex.value);
-    fileIndex.value = swiper.activeIndex;
-    if (fileIndex.value == cachedImages.value.length - 1) {
-        getNextImages().forEach(i => cachedImages.value?.push(i));
-    }
 
+    const isNext = fileIndex.value < swiper.activeIndex;
+
+    fileIndex.value = swiper.activeIndex;
+    if (isNext && cachedImages.value.at(fileIndex.value + 1) == undefined) {
+        populateImageFiles(getImages(true, fileIndex.value, false));
+    }
+    else if (cachedImages.value.at(fileIndex.value - 1) == undefined) {
+        populateImageFiles(getImages(false, fileIndex.value, false));
+    }
+    imageStore.setLastEvent(EventSource.swipe, fileIndex);
     isLoading.value = false;
 }
 
@@ -71,11 +110,16 @@ function toggleFullScreen() {
     fullScreenMode.value = !fullScreenMode.value;
 }
 
+function initSwiper(swiper: SwiperCore) {
+    swiperInstance.value = swiper;
+}
+
 </script>
 <template>
-    <swiper ref="swiper" :slides-per-view="1"  :space-between="50" @active-index-change="loadImages" :virtual="true">
-        <swiper-slide v-for="imageFile in cachedImages" :virtual-index="imageFile.index" :swiper-ref="SwiperCore" :ref_key="imageFile.fileName">
-            <a href="#" aria-role-description="button" role="button" @click="toggleFullScreen">
+    <swiper @init="initSwiper" :slides-per-view="1" :space-between="50" @active-index-change="loadImages" :virtual="true">
+        <swiper-slide v-for="imageFile in cachedImages?.filter(f => f != undefined)" :virtual-index="imageFile.index"
+            :swiper-ref="SwiperCore" :ref_key="imageFile.fileName">
+            <a href="#" aria-role-description="button" role="button" :data-type="imageFile.index" @click="toggleFullScreen">
                 <img :class="setImageClass()" :src="imageFile.fileName" :alt="imageFile.name" />
             </a>
         </swiper-slide>
